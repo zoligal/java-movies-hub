2f590046218121ffc92617624ea79c7d63533c3e
zoligal <zoligal@yandex.ru>
2026-09-10 18:01:29 +0300

    убрал неиспользуемый импорт ErrorResponse

--- src/main/ru/practicum/moviehub/http/MoviesHandler.java
+++ src/main/ru/practicum/moviehub/http/MoviesHandler.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import ru.practicum.moviehub.api.ErrorResponse;
 import ru.practicum.moviehub.model.Movie;
 import ru.practicum.moviehub.store.MoviesStore;
 
